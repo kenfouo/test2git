7fd124835f95df437baaa38f8e240d97244b655f
Borient Manfouo <borientm@yahoo.fr>
2022-01-20 14:10:53 +0100

    optimisation

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var item1    = document.getElementById('item1');
 var selected = document.getElementById('selected');
 
 item1.onclick = function () {
-    selected.classList.remove('switchss'); 
+    selected.classList.remove('switch'); 
 }
 item2.onclick = function () {
-    selected.classList.add('switchss'); 
+    selected.classList.add('switch'); 
 }
