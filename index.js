00f9f323286615b56a0c0bf64156b895d38088e4
Borient Manfouo <borientm@yahoo.fr>
2022-01-25 15:44:52 +0100

    updates

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 
-var item1    = document.getElementById('item1');
-var selected = document.getElementById('selected');
+var item1       = document.getElementById('item1');
+var selected    = document.getElementById('selected');
+var myStickers  = document.getElementById('myStickers');
+var allStickers = document.getElementById('allStickers');
 
 item1.onclick = function () {
     selected.classList.remove('switch'); 
+    setTimeout(() => {
+        allStickers.style.display = 'flex'
+        myStickers.style.display = 'none'
+    }, 400);
 }
 item2.onclick = function () {
     selected.classList.add('switch'); 
+    setTimeout(() => {
+        myStickers.style.display = 'flex'
+        allStickers.style.display = 'none'
+    }, 400);
 }
